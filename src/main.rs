74b09a85391dd371693b58bcb433162e192df57f
csg001 <937496569@qq.com>
2022-05-04 18:43:43 +0800

    enum_vctor_println!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+enum test_enum {
+    A(i32),
+    B(f32),
+    C(String),
+}
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
-    println!("Hello, world!");
+    let mut c = vec![
+        test_enum::A(3),
+        test_enum::B(3.0),
+        test_enum::B(3.6),
+        test_enum::C("T".to_string()),
+    ];
+    //let mut v=vec![0,502,-502];
+    for i in &mut c {
+        //*i +=50;
+        match i {
+            test_enum::A(a) => println!("{}",a),
+            test_enum::B(b) => println!("{}", b),
+            test_enum::C(c) => println!("{}", c),
+        }
+    }
 }
